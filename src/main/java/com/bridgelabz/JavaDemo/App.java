cc76a4999a51cdef72d632741bc98ba64297849f
BL-Madhukar <104752502+BL-Madhukar@users.noreply.github.com>
2022-07-21 20:15:09 +0530

    File changed in remote

--- src/main/java/com/bridgelabz/JavaDemo/App.java
+++ src/main/java/com/bridgelabz/JavaDemo/App.java
@@ -13,10 +13,10 @@ public class App
     public static void main( String[] args )
     {
     	String message = "Hello Java";
-    	LOG.debug(message, " will be printed on debug 1");
-    	LOG.info(message, " will be printed on info 1");
-    	LOG.warn(message, " will be printed on warn 1");
-    	LOG.error(message, " will be printed on error 1");
+    	LOG.debug(message, " will be printed on debug 3");
+    	LOG.info(message, " will be printed on info 3");
+    	LOG.warn(message, " will be printed on warn 3");
+    	LOG.error(message, " will be printed on error 3");
     	LOG.fatal(message, " will be printed on fatal 1");
         System.out.println("Hello world");
     }
