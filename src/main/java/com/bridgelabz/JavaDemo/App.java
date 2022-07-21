f5948c3afdd6972a3a95f25efe678029c4d134fc
BL-Madhukar <madhukar.tatiraju@bridgelabz.com>
2022-07-21 20:14:23 +0530

    File changed in local

--- src/main/java/com/bridgelabz/JavaDemo/App.java
+++ src/main/java/com/bridgelabz/JavaDemo/App.java
@@ -13,10 +13,10 @@ public class App
     public static void main( String[] args )
     {
     	String message = "Hello Java";
-    	LOG.debug(message, " will be printed on debug 1");
-    	LOG.info(message, " will be printed on info 1");
-    	LOG.warn(message, " will be printed on warn 1");
-    	LOG.error(message, " will be printed on error 1");
+    	LOG.debug(message, " will be printed on debug 2");
+    	LOG.info(message, " will be printed on info 2");
+    	LOG.warn(message, " will be printed on warn 2");
+    	LOG.error(message, " will be printed on error 2");
     	LOG.fatal(message, " will be printed on fatal 1");
         System.out.println("Hello world");
     }
